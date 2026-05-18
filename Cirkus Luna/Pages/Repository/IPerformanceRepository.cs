beae8e5c240681d378432637bb9081cb47ab1b3c
DetTrine <trineweitmann@gmail.com>
2026-05-18 10:37:36 +0200

    with performance

---
+++ Cirkus Luna/Pages/Repository/IPerformanceRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Eksamen.Repository;
+using Eksamen.Model;
+
+    public interface IPerformanceRepository
+    {
+        public List<Performance> GetAll();
+        public Performance GetById(int id);
+
+        public void Add(Performance performance);
+        public void Update(Performance performance);
+
+        public void Delete(Performance performance);
+
+    }
+
